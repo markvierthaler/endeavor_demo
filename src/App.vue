da06977b57f7914b186cc11079f6b9aed14b315c
MarkVierthaler <mark.vierthaler@gmail.com>
2021-06-29 12:39:10 -0500

    third commit - 6/29/2021 12:39

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Category 
+      v-bind:category=categories[0] 
+      v-bind:currentWidth=currentWidth
+      v-bind:breakpoint=breakpoint
+      v-bind:listLimit=listLimit
+      />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Category from './components/Category';
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Category
+  },
+  data() {
+    return {
+      listLimit: "4",
+      currentWidth:"800",
+      breakpoint: "800",
+      categories: [
+        {
+          label: "FEATURED BLOGS",
+          categoryUrl: "/blog",
+          items: [
+            {
+              id: 1,
+              title: "Test Article 1",
+              summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              itemUrl: "https://www.endeavorbusinessmedia.com/",
+              pubDate:  new Date("06/27/2021")
+            },
+            {
+              id: 2,
+              title: "Test Article 2",
+              summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              itemUrl: "https://www.endeavorbusinessmedia.com/",
+              pubDate:  new Date("06/27/2021")
+            },
+            {
+              id: 3,
+              title: "Test Article 3",
+              summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              itemUrl: "https://www.endeavorbusinessmedia.com/",
+              pubDate:  new Date("06/26/2021")
+            },
+            {
+              id: 4,
+              title: "Test Article 4",
+              summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              itemUrl: "https://www.endeavorbusinessmedia.com/",
+              pubDate:  new Date("06/26/2021")
+            },
+            {
+              id: 5,
+              title: "Test Article 5",
+              summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              itemUrl: "https://www.endeavorbusinessmedia.com/",
+              pubDate:  new Date("06/25/2021")
+            },
+            {
+              id: 6,
+              title: "Test Article 6",
+              summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              itemUrl: "https://www.endeavorbusinessmedia.com/",
+              pubDate:  new Date("06/24/2021")
+            },
+            {
+              id: 7,
+              title: "Test Article 7",
+              summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              itemUrl: "https://www.endeavorbusinessmedia.com/",
+              pubDate:  new Date("06/24/2021")
+            }
+            ]
+        }  
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    font-family: Arial, Arial, Helvetica, sans-serif;
+  }
 </style>
